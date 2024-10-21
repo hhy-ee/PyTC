8cdece3f58ea4a9eee6215b1ad80ffd4d7c3873f
ps-L40S <123@sjtu.edu.cn>
2024-10-21 15:12:19 +0800

    1

--- scripts/main.py
+++ scripts/main.py
@@ -40,8 +40,12 @@ def get_args():
         nargs=argparse.REMAINDER,
     )
     args = parser.parse_args()
-    args.config_file = './configs/MitoEM/MitoEM-R-3d-BC-reso1e0-scale1e-0.yaml'
-    args.config_base = './configs/MitoEM/MitoEM-Base.yaml'
+    # args.config_file = './configs/MitoEM/MitoEM-R-3d-BC-reso1e0-scale1e-0.yaml'
+    # args.config_base = './configs/MitoEM/MitoEM-Base.yaml'
+
+    args.config_file = './configs/SNEMI/SNEMI-Affinity-UNet.yaml'
+    args.config_base = './configs/SNEMI/SNEMI-Base.yaml'
+
     return args
 
 def main():
